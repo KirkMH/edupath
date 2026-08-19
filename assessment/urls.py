adf1b425ef06f4279961b22c4cfddf203b2fec98
KirkMH <kirkhilario@gmail.com>
2026-08-20 06:10:54 +0800

    Added the templates

--- assessment/urls.py
+++ assessment/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
     path('dashboard/', views.dashboard, name='dashboard'),
-]
+    path('aptitude-ready/', views.aptitude_ready, name='aptitude_ready'),
+    path('assess-aptitude/', views.assess_aptitude, name='assess_aptitude'),
+    path('preference-ready/', views.preference_ready, name='preference_ready'),
+    path('assess-preference/', views.assess_preference, name='assess_preference'),
+    path('result/', views.result, name='result'),
+]
--- assessment/views.py
+++ assessment/views.py
@@ -11,4 +11,23 @@ def signin(request):
 
 def dashboard(request):
     return render(request, 'assessment/dashboard.html')
-    
+
+
+def aptitude_ready(request):
+    return render(request, 'assessment/aptitude-ready.html')
+
+
+def assess_aptitude(request):
+    return render(request, 'assessment/aptitude-assess.html')
+
+
+def preference_ready(request):
+    return render(request, 'assessment/preference-ready.html')
+
+
+def assess_preference(request):
+    return render(request, 'assessment/preference-assess.html')
+
+
+def result(request):
+    return render(request, 'assessment/result.html')
